36fbfa4c46aa7064383b320b91d07a386519e887
Owen <31156070+imowen@users.noreply.github.com>
2025-09-27 14:42:26 +0700

    Update vite.config.ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -9,7 +9,7 @@ import Components from 'unplugin-vue-components/vite'
 import { defineConfig, loadEnv } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { VitePWA } from 'vite-plugin-pwa'
-import { VitePluginRadar } from 'vite-plugin-radar'
+
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 const base = process.env.SERVER_ENV === `NETLIFY` ? `/` : `/md/`
@@ -67,9 +67,7 @@ export default defineConfig(({ mode }) => {
         // fs: 'memfs',
         },
       }),
-      VitePluginRadar({
-        analytics: { id: `G-7NZL3PZ0NK` },
-      }),
+      
       ...(process.env.ANALYZE === `true` ? [visualizer({ emitFile: true, filename: `stats.html` }) as any] : []),
       AutoImport({
         imports: [`vue`, `pinia`, `@vueuse/core`],
